4b8c94e15bb78805b9243d2dc059e0a0d7908a0d
ameykhaire <ameybro11@gmail.com>
2020-07-13 22:26:03 +0530

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const middlewares = require('./middlewares');
-const api = require('./api');
+const middlewares = require("./middlewares");
+const api = require("./api");
 
 const app = express();
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 
-app.use(morgan('dev'));
+app.use(cors());
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
-  });
+app.get("/", (req, res) => {
+    res.json({
+        message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+    });
 });
 
-app.use('/api/v1', api);
+app.use("/api/v1", api);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
